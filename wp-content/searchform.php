64e442d145dac3903e613f96d5eeea0544e123eb
dd86k <dd@dax.moe>
2023-05-21 13:04:00 -0400

    Fix #3 - search input query

--- wp-content/searchform.php
+++ wp-content/searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" id="searchform" class="searchform">
 <table>
 <tr>
-<td><input type="text" name="s" id="s" placeholder="SEARCH HERE TODAY!"></td>
+<td><input type="text" name="s" id="s" placeholder="SEARCH POSTS" value="<?php echo get_search_query(); ?>"></td>
 <td><input type="submit" id="searchsubmit" value="SEARCH"></td>
 </tr>
 </table>
