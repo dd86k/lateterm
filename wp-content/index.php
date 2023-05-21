3d0119feb1e210129d348bad29bfd42b6f20b9f6
dd86k <dd@dax.moe>
2023-05-21 15:27:06 -0400

    Fix search

--- wp-content/index.php
+++ wp-content/index.php
@@ -1,18 +1,24 @@
-<?php get_header(); ?>
-<div class="posts">
-<?php
+<?php get_header();
+
+echo('<div class="posts">');
+
 if (is_search()) {
-	esc_html__( 'Search Results for: %s', 'cock');
-} elseif (have_posts()) {
+	echo('<h2>Search results for: "'.get_search_query().'"</h2>');
+}
+if (have_posts()) {
+	echo('<table>');
 	 while (have_posts()) {
-		 the_post(); ?>
-	<table>
-	<td><?php the_time('Y-m-d') ?></td>
-	<td><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></td>
-	</table>
-<?php 	}
- } else { ?>
-	<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php } ?>
-</div>
-<?php get_footer(); ?>
+		the_post();
+		echo('<tr>');
+		echo('<td>'.get_the_time('Y-m-d').'</td>');
+		echo('<td><a href="'.get_the_permalink().'">'.get_the_title().'</a></td>');
+		echo('</tr>');
+	}
+	echo('</table>');
+} else {
+	echo('<p>'._e('Sorry, no posts matched your criteria.').'</p>');
+}
+
+echo('</div>');
+
+get_footer(); ?>
