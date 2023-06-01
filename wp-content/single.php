01d13ea07a2d07108535d504847d92cabafbe643
dd86k <dd@dax.moe>
2023-06-01 16:44:13 -0400

    Implement #12 and #11

--- wp-content/single.php
+++ wp-content/single.php
@@ -1,8 +1,21 @@
 <?php get_header(); the_post(); ?>
 <header>
 <h1><?php the_title(); ?></h1>
-<?php the_time(get_option('date_format')); ?><br>
-<?php the_author(); ?>
+<div>Author: <?php the_author(); ?></div>
+<div>Published: <?php the_time(get_option('date_format')); ?></div>
+<?php
+$u_time          = get_the_time( 'U' );
+$u_modified_time = get_the_modified_time( 'U' );
+// Only display modified date if 24hrs have passed since the post was published.
+if ( $u_modified_time >= $u_time + 86400 ) {
+ 
+    $updated_date = get_the_modified_date();
+    $updated_time = get_the_modified_time();
+    
+    echo('<div>Last modified: '.$updated_date.' at '.$updated_time.'</div>');
+}
+?>
+<div>Categories: <?php the_category(); ?></div>
 </header>
 <div><?php the_content(); ?></div>
 <div class="singlenav">
