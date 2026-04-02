28a801d326be7e39180a5ff67b1648bb20b6a5e7
dd86k <dd@dax.moe>
2026-04-02 15:43:50 -0400

    Scroll-to-top button (fixes #7)

--- wp-content/footer.php
+++ wp-content/footer.php
@@ -1,6 +1,15 @@
 </div>
 </div>
 </div>
+<a href="#" id="back-to-top" title="Back to top">&uarr;</a>
+<script>
+(function() {
+	var btn = document.getElementById('back-to-top');
+	window.addEventListener('scroll', function() {
+		btn.style.display = window.scrollY > 300 ? 'block' : 'none';
+	});
+})();
+</script>
 <?php wp_footer(); ?>
 </body>
 </html>
