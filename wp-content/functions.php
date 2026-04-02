9c8a8a13f3c86371e973d72308ae168af32c4558
dd86k <dd@dax.moe>
2026-04-02 16:10:29 -0400

    Block Editor support (completes #16)

--- wp-content/functions.php
+++ wp-content/functions.php
@@ -1,4 +1,14 @@
 <?php
+function lateterm_setup() {
+    add_theme_support( 'editor-styles' );
+    add_theme_support( 'wp-block-styles' );
+    add_theme_support( 'responsive-embeds' );
+    add_theme_support( 'align-wide' );
+    add_editor_style( 'style.css' );
+    add_editor_style( 'editor-style.css' );
+}
+add_action( 'after_setup_theme', 'lateterm_setup' );
+
 function lateterm_customize_theme_options_register( $wp_customize ) {
     // SECTION 1: Theme options
     $wp_customize->add_section( 'lateterm_options', array(
@@ -140,6 +150,43 @@ add_action( 'customize_register', 'lateterm_customize_theme_show_register' );
 function lateterm_sanitize_checkbox( $checked ) {
     return ( isset( $checked ) && true == $checked ) ? true : false;
 }
+
+// Pass Customizer values into the block editor
+function lateterm_editor_customizer_styles() {
+    $bg_color    = esc_attr( get_theme_mod( 'lateterm_bg_color', '#000084' ) );
+    $font_color  = esc_attr( get_theme_mod( 'lateterm_font_color', '#ffffff' ) );
+    $font_family = esc_attr( get_theme_mod( 'lateterm_font_family', 'monospace' ) );
+    $font_size   = esc_attr( get_theme_mod( 'lateterm_font_size', '18px' ) );
+    $max_width   = esc_attr( get_theme_mod( 'lateterm_max_width', '70em' ) );
+
+    $css = "
+        .editor-styles-wrapper {
+            background-color: {$bg_color} !important;
+            color: {$font_color} !important;
+            font-family: {$font_family} !important;
+            font-size: {$font_size} !important;
+        }
+        .editor-styles-wrapper a {
+            color: {$font_color} !important;
+        }
+        .editor-styles-wrapper h1,
+        .editor-styles-wrapper h2,
+        .editor-styles-wrapper h3,
+        .editor-styles-wrapper h4,
+        .editor-styles-wrapper h5,
+        .editor-styles-wrapper h6 {
+            color: {$font_color} !important;
+        }
+        .editor-styles-wrapper .wp-block {
+            max-width: {$max_width};
+        }
+    ";
+
+    wp_register_style( 'lateterm-editor-customizer', false );
+    wp_enqueue_style( 'lateterm-editor-customizer' );
+    wp_add_inline_style( 'lateterm-editor-customizer', $css );
+}
+add_action( 'enqueue_block_editor_assets', 'lateterm_editor_customizer_styles' );
 
 // Add id attributes to content headers for anchor links
 function lateterm_add_header_ids( $content ) {
--- wp-content/page.php
+++ wp-content/page.php
@@ -2,7 +2,7 @@
 <header>
 <h1><?php the_title(); ?></h1>
 </header>
-<p><?php the_content(); ?></p>
+<div><?php the_content(); ?></div>
 <hr>
 <?php
 get_sidebar();
